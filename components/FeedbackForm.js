12d236e7ee0c39e9e11a684cb6a6aa0cfc85a459
neeraj <me.neeraj1455@gmail.com>
2023-10-10 16:15:45 +0530

    manage vertual keyboard

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -1,40 +1,59 @@
 import React, { useState, useEffect } from "react";
-import { ScrollView, Text, TextInput, StyleSheet } from "react-native";
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 
 function FeedbackForm() {
   const [firstName, onChangeFirstName] = useState("");
   const [lastName, onChangeLastName] = useState("");
+  const [phoneNumber, onChangePhoneNumber] = useState("");
   const [message, onChangeMessage] = useState("");
 
   return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.headingSection}>
-        How was your visit to Little Lemon?
-      </Text>
-      <Text style={styles.infoSection}>
-        Little Lemon is a charming neighborhood bistro that serves simple food  
-        and classic cocktails in a lively but casual environment. We would love
-        to hear your experience with us!
-      </Text>
-      <TextInput
-        style={styles.input}
-        value={firstName}
-        placeholder="First Name"
-        onChangeText={onChangeFirstName}
-      ></TextInput>
-      <TextInput
-        style={styles.input}
-        value={lastName}
-        placeholder="Last Name"
-        onChangeText={onChangeLastName}
-      ></TextInput>
-      <TextInput
-        style={styles.messageInput}
-        value={message}
-        placeholder="Message"
-        onChangeText={onChangeMessage}
-      ></TextInput>
-    </ScrollView>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "Android" ? "padding" : "height"}
+    >
+      <ScrollView keyboardDismissMode="on-drag">
+        <Text style={styles.headingSection}>
+          How was your visit to Little Lemon?
+        </Text>
+        <Text style={styles.infoSection}>
+          Little Lemon is a charming neighborhood bistro that serves simple food
+            and classic cocktails in a lively but casual environment. We would
+          love to hear your experience with us!
+        </Text>
+        <TextInput
+          style={styles.input}
+          value={firstName}
+          placeholder="First Name"
+          onChangeText={onChangeFirstName}
+        ></TextInput>
+        <TextInput
+          style={styles.input}
+          value={lastName}
+          placeholder="Last Name"
+          onChangeText={onChangeLastName}
+        ></TextInput>
+        <TextInput
+          style={styles.input}
+          value={phoneNumber}
+          placeholder="Phone Number"
+          onChageText={onChangePhoneNumber}
+        ></TextInput>
+        <TextInput
+          style={styles.messageInput}
+          value={message}
+          placeholder="Message"
+          onChangeText={onChangeMessage}
+        ></TextInput>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -67,7 +86,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     fontSize: 16,
-    borderColor: "EDEFEE",
+    borderColor: "#EDEFEE",
     backgroundColor: "white",
   },
   messageInput: {
