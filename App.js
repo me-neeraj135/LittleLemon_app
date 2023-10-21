0ca0138bfaf90b09c8090044b408c21b71a5eb6b
neeraj <me.neeraj1455@gmail.com>
2023-10-21 16:42:53 +0530

    add images

--- App.js
+++ App.js
@@ -3,19 +3,21 @@ import { StyleSheet, Text, View } from "react-native";
 
 import MenuItems from "./components/MenuItems.js";
 import LoginPage from "./components/LoginPage.js";
+import WelcomeScreen from "./components/WelcomeScreen.js";
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
-        <LoginPage />
+        {/* <LoginPage /> */}
         {/* <MenuItems /> */}
+        <WelcomeScreen />
       </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "#495E57" },
+  container: { flex: 1, padding: 20 },
   footer: { backgroundColor: "#495E57" },
 });
--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -1,16 +1,55 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  ScrollView,
+  Text,
+  StyleSheet,
+  Image,
+  ImageBackground,
+} from "react-native";
 
+const logo = require("../assets/images/littleLogo.png");
 function WelcomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to Little Lemon</Text>
-      <Text style={styles.discription}>
-        Little Lemon is a charming neighborhood bistro that serves simple food
-        and classic cocktails in a lively but casual environment. We would love
-        to hear more about your experience with us!
+    <ScrollView style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={logo}
+        resizeMode="contain"
+        accessible={true}
+        accessibilityLabel={"Little Lemon Logo"}
+      />
+      <Text style={styles.title}>
+        Little Lemon, your local Mediterranean Bistro
       </Text>
-    </View>
+      <Image
+        style={styles.img}
+        source={require("../assets/images/brooke-lark-HjWzkqW1dgI-unsplash.jpg")}
+        resizeMode="cover"
+        accessible={true}
+        accessibilityLabel={"food picture1"}
+      />
+      <Image
+        style={styles.img}
+        source={require("../assets/images/edgar-castrejon-1SPu0KT-Ejg-unsplash.jpg")}
+        resizeMode="cover"
+        accessible={true}
+        accessibilityLabel={"food picture2"}
+      />
+      <Image
+        style={styles.img}
+        source={require("../assets/images/mekht-GuvimT4IFok-unsplash.jpg")}
+        resizeMode="cover"
+        accessible={true}
+        accessibilityLabel={"food picture3"}
+      />
+      <Image
+        style={styles.img}
+        source={require("../assets/images/wikisleep-app-qvIrI4ueqzY-unsplash.jpg")}
+        resizeMode="cover"
+        accessible={true}
+        accessibilityLabel={"food picture4"}
+      />
+    </ScrollView>
   );
 }
 
@@ -18,12 +57,21 @@ export default WelcomeScreen;
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
-  title: { padding: 40, fontSize: 30, color: "#EDEFEE", textAlign: "center" },
-  discription: {
-    fontSize: 24,
-    padding: 20,
-    marginVertical: 8,
-    color: "#EDEFEE",
-    textAlign: "center",
+  logo: {
+    height: 100,
+    width: 300,
+    marginTop: 30,
+  },
+  title: { padding: 20, fontSize: 22, textAlign: "center", fontWeight: `600` },
+  img: {
+    height: 200,
+    width: `100%`,
+    borderRadius: 5,
+    marginTop: 10,
+    justifyContent: "center",
+  },
+  bgImg: {
+    flex: 1,
+    justifyContent: `center`,
   },
 });
